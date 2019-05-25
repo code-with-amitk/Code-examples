9da8acc16967755e95cd7235920677dd15d3abc5
AmitKumar <amitkumar50@users.noreply.github.com>
2019-05-25 16:07:44 +0530

    Update isBSTorNot.c

--- data-structures/trees/binary-search-tree/isBSTorNot.c
+++ data-structures/trees/binary-search-tree/isBSTorNot.c
@@ -2,61 +2,116 @@
  * 	5
  *     / \
  *    3	  6
- *   / \
- *  2	4 
+ *   / \    \
+ *  2	4     1
  */
 #include<stdio.h>
 #include<stdlib.h>	//malloc
-typedef struct node {
-	int val;
-	struct node *left;
-	struct node *right;
+typedef struct Node {
+        int data;
+        struct Node *left;
+        struct Node *right;
 }NODE;
 
 NODE *createBST (){
-	NODE *ptr;
-	ptr = (NODE *) malloc (sizeof(NODE));
-	ptr->val = 5;
-	ptr->left = (NODE *) malloc (sizeof(NODE));
-	ptr->left->val = 3;
-	ptr->left->left = (NODE *) malloc (sizeof(NODE));
-	ptr->left->right = (NODE *) malloc (sizeof(NODE));
-	ptr->left->left->left = ptr->left->left->right = NULL;
-	ptr->left->left->val = 2;
-	ptr->left->right->val = 4;
-	ptr->left->right->left = ptr->left->right->right = NULL;
+        NODE *ptr;
+        ptr = (NODE *) malloc (sizeof(NODE));
+        ptr->data = 5;
+        ptr->left = (NODE *) malloc (sizeof(NODE));
+        ptr->left->data = 3;
+        ptr->left->left = (NODE *) malloc (sizeof(NODE));
+        ptr->left->right = (NODE *) malloc (sizeof(NODE));
+        ptr->left->left->left = ptr->left->left->right = NULL;
+        ptr->left->left->data = 2;
+        ptr->left->right->data = 4;
+        ptr->left->right->left = ptr->left->right->right = NULL;
 
-	ptr->right = (NODE *) malloc (sizeof(NODE));
-	ptr->right->val = 6;
-	ptr->right->left = NULL;
-	ptr->right->right = NULL;
+        ptr->right = (NODE *) malloc (sizeof(NODE));
+        ptr->right->data = 6;
+        ptr->right->left = NULL;
+        ptr->right->right = (NODE *) malloc (sizeof(NODE));
+        ptr->right->right->data = 1;
+        ptr->right->right->right = NULL;
+        ptr->right->right->left = NULL;
 
-	return ptr;
+        return ptr;
 }
 
-int isBST (NODE *ptr, int parentVal, int isLeft, int isRight) {
-	int temp;
-	if(isLeft && (ptr->val > parentVal))
-		return 1;
-	if(isRight && (ptr->val < parentVal))
-		return 1;
-
-	if (ptr->left) 
-		temp = isBST(ptr->left,ptr->val,1,0);
-	if (ptr->right) 
-		temp = isBST(ptr->right,ptr->val,0,1);
-	return temp;
+NODE * findMaximum(NODE * root){
+    if( !root ) return root;
+    while( root->right ){
+        root = root->right;
+    }
+    return root;
 }
+
+NODE * findMinimum(NODE * root){
+    if( !root ) return root;
+    while( root->left ){
+        root = root->left;
+    }
+    return root;
+}
+
+//return=0 (if not BST)
+//return=1 (if BST)
+int isBST_logic_1 (NODE *root) {
+    if(!root)
+        return 1;
+
+    if( ! ( root->left || root->right ) ) return 1;
+    int isLeft  = isBST_logic_1(root->left);
+    int isRight = isBST_logic_1(root->right);
+
+    if(isLeft && isRight){
+        /* Since we already know that left sub tree and
+        *  right sub tree are Binary search tree, finding min and max in them would be easy */
+
+        NODE *max  =  NULL;
+        NODE *min  =  NULL;
+        if( root->left )
+            max = findMaximum(root->left);
+        if( root->right )
+            min = findMinimum(root->right);
+         if(max && !min)
+            return root->data > max->data;
+        if(!max && min)
+            return root->data < min->data;
+        return (root->data > max->data && root->data < min->data);
+   }
+   return 0;
+}
+
+int isBST_logic_2 (NODE *root) {
+        int ret;
+        if((root->left) && (root->left->data > root->data))     //Not BST
+                return 0;
+
+        if((root->right) && (root->right->data < root->data))   //Not BST
+                return 0;
+
+        if (((root->left) && !(isBST_logic_2(root->left))) || (root->right && !(isBST_logic_2(root->right))))
+                return 0;
+
+        return 1;
+}
+
 
 void main() {
-	NODE *root = createBST();
-	printf("Tree created\n");
+        NODE *root = createBST();
+        printf("Tree created\n");
 
-	if(isBST(root, 5, 0, 0)) {
-		printf("Not BST\n");
-	} else {
-		printf("Tree is BST\n");
-	}
+        if(isBST_logic_1(root)) {
+                printf("BST\n");
+        } else {
+                printf("Not BST\n");
+        }
+	
+        if(isBST_logic_2(root)) {
+                printf("BST\n");
+        } else {
+                printf("Not BST\n");
+        }
 }
 
 /* Output:
