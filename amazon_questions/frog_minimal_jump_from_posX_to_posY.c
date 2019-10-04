7e30f2c0db73a2b6267f0d4c659c441f9fcbb197
AmitKumar <amitkumar50@users.noreply.github.com>
2019-10-04 11:50:48 +0530

    Create frog_minimal_jump_from_posX_to_posY.c

---
+++ amazon_questions/frog_minimal_jump_from_posX_to_posY.c
@@ -0,0 +1,41 @@
+/*
+A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+
+Count the minimal number of jumps that the small frog must perform to reach its target.
+
+Write a function:
+
+int solution(int X, int Y, int D);
+
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+
+For example, given:
+
+  X = 10
+  Y = 85
+  D = 30
+the function should return 3, because the frog will be positioned as follows:
+
+after the first jump, at position 10 + 30 = 40
+after the second jump, at position 10 + 30 + 30 = 70
+after the third jump, at position 10 + 30 + 30 + 30 = 100
+Write an efficient algorithm for the following assumptions:
+
+X, Y and D are integers within the range [1..1,000,000,000];
+X ≤ Y.
+ */
+#include<stdio.h>
+int solution(int x, int y, int d){
+        int count=0;
+        while(x<y){
+                if(y>x){
+                        count++;
+                        x = x+d;
+                }
+        }
+        return count;
+}
+void main(){
+        int x=10,y=85,d=30;
+        printf("%d\n",solution(x,y,d));
+}
