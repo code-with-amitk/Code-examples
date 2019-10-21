fa82379676751b7fa69ac13308432f32a66c2fc1
AmitKumar <amitkumar50@users.noreply.github.com>
2019-10-21 10:31:47 +0530

    Create restful_API_GetPostPutDel.py

---
+++ python/restful_API_GetPostPutDel.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
+
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "User not found", 404
+
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted.".format(name), 200
+
+api.add_resource(User, "/user/<string:name>")
+
+app.run(debug=True)
+
+
+
+#2. Accessing APIs using Clients
+#    a. Using client Insomnia
+#    - Install "Insomnia" client application to send RestAPI calls in chrome. Access it using (chrome://apps/)
+#        GET: 127.0.0.1:5000/user/Elvin
+#        POST: 127.0.0.1:5000/user/Amit 
+#        DELETE: 127.0.0.1:5000/user/Amit
+#    b. Using firefox web Browser:        http://127.0.0.1:5000/user/Elvin
+#    c. Using Python Code    https://www.geeksforgeeks.org/get-post-requests-using-python/
