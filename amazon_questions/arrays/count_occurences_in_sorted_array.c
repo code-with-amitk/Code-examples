fe154684efe44da79345128f0f717525a0c21fcb
AmitKumar <amitkumar50@users.noreply.github.com>
2019-11-14 08:57:03 +0530

    Create count_occurences_in_sorted_array.c

---
+++ amazon_questions/arrays/count_occurences_in_sorted_array.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+/* Logic-1: Traverse array linearly count number of occurrences of element. Complexity=O(n)
+ *
+ * Logic-2: Binary search + Linear count.         O(logn) + count
+ *  -   Find element using binary search.
+ *  -   Once element is found count left, right.
+ *
+ * Logic-3(Binary Search): Complexity=O(logn)
+ *  - Traverse in binary search manner.
+ *  - get index of the first occurrence of x in arr[]. Let the index of the first occurrence be i.
+ *  - get index of the last occurrence of x in arr[]. Let the index of the last occurrence be j.
+ *  - Output  (j - i)
+*/
+int BinarySearch(int A[], int sizeofA, int element_to_find, int leftOrRightSearch)
+{
+        int low = 0, high = sizeofA - 1, result = -1;
+
+        while (low <= high)
+        {
+                int mid = (low + high)/2;
+
+                if (element_to_find == A[mid])
+                {
+                        result = mid;
+                        if (leftOrRightSearch){         //Let's search left occurence of element from mid
+                                high = mid - 1;
+                        }else{
+                                low = mid + 1;
+                        }
+                }else if (element_to_find < A[mid]){
+                        high = mid - 1;
+                }else{
+                        low = mid + 1;
+                }
+        }
+
+        return result;
+}
+
+int main(void)
+{
+//      int A[] = {2, 5, 5, 5, 6, 6, 8, 9, 9, 9}, to_find=5;
+        int A[] = {1,1,1,1,2,2,3}, to_find = 2;
+
+        int sizeofA = sizeof(A)/sizeof(A[0]);
+
+        int first = BinarySearch(A, sizeofA, to_find, 1);  // 1 for first occurrence
+        int last = BinarySearch(A, sizeofA, to_find, 0);   // 0 for last occurrence
+
+        int count = last - first + 1;
+
+        if (first != -1){
+                printf("Element %d occurs %d times.", to_find, count);
+        }else{
+                printf("Element not found in the array.");
+        }
+        return 0;
+}
