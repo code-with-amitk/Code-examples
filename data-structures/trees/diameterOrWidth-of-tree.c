c017f80c4b7a8c86f3471011f0670532625e7376
AmitKumar <amitkumar50@users.noreply.github.com>
2019-12-21 12:04:48 +0530

    Update and rename diameter-of-tree.c to diameterOrWidth-of-tree.c

--- data-structures/trees/diameterOrWidth-of-tree.c
+++ data-structures/trees/diameterOrWidth-of-tree.c
@@ -7,16 +7,30 @@
  *   4   5
  *  / 	  \
  * 8  	   10
- * Here: Diameter is 4 between 8 & 10, This does not passed through ROOT
+ Here: Diameter is 5 between 8 & 10. Nodes are counted in diameter.
+ 8,4,2,5,10 -> 5 nodes. Diameter 5.
+ 
+ Logic:	
+ diameter = max ((leftHeight + rightHeight + 1) , max(leftDiameter, rightDiameter))
+ 
+ So, 4 things to be calculated:
+ 	a. leftHeight
+	b. rightHeight
+	c. leftDiameter
+	d. rightDiameter
  */
 #include<stdio.h>
 #include<stdlib.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
 typedef struct node {
 	int val;
 	struct node *right;
 	struct node *left;
 }NODE;
-int lv,rv;
+
 NODE *create() {
 	NODE *ptr = (NODE *) malloc (sizeof(NODE));
 	ptr->val = 1;	ptr->right = NULL;	
@@ -32,23 +46,26 @@ NODE *create() {
 	ptr->left->right->right = (NODE *) malloc (sizeof(NODE));
 	ptr->left->right->right->val=10; ptr->left->right->right->left=NULL;
 	ptr->left->right->right->right=NULL;
+	return ptr;
+}
 
+int height(NODE *node){
+        if (node == NULL)return 0;
+        return 1 + max(height(node->left), height(node->right));	
 }
-int fD(NODE *ptr){
-#if 0	//Wrong logic. TBD
-	if(ptr->left) {
-		lv++;
-		lv = fD(ptr->left);
-	}
-	if(ptr->right) {
-		rv++;
-		rv = fD(ptr->right);
-	}
-	return ((lv>rv)?lv:rv);
-#endif
+
+int diameter(NODE *ptr){
+	if(ptr == NULL) return 0;
+	int leftHeight = height(ptr->left);
+	int rightHeight = height(ptr->right);
+	int leftDiameter = diameter(ptr->left);
+	int rightDiameter = diameter(ptr->right);
+	
+	return max((leftHeight + rightHeight + 1), max(leftDiameter, rightDiamter));
 }
+
 void main(){
 	NODE *root = create();
 	printf("Tree Created\n");
-	printf("Diameter=%d\n",fD(root));	//findDiameter
+	printf("Diameter=%d\n",diameter(root));	//findDiameter
 }
