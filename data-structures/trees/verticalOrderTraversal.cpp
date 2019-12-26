b0c970d1fc7843eb627ac66a6fc6db5c16dcf038
AmitKumar <amitkumar50@users.noreply.github.com>
2019-12-26 12:04:49 +0530

    Update verticalOrderTraversal.cpp

--- data-structures/trees/verticalOrderTraversal.cpp
+++ data-structures/trees/verticalOrderTraversal.cpp
@@ -1,121 +1,146 @@
 /*
-                1
-              /   \
-             2     3
-            / \   / \
-           4   5  6  7
-                    / \
-                   8   9
-Vertical Order Traversal of Tree? Nodes printed after traversing the tree vertically.
-Calculate VO?
-        1. Calculate Horizontal Distance(Hd) of nodes from Root
-                -> Hd: root(0), left-node(parent-1), right-node(parent+1)
-        2. Nodes having same Hd are on same vertical Line.
-                -> Print nodes having horizontal distance as (-2,-1,0,1,2..)
-                -> Vertically Traversed: 4, 2, 1, 5, 6, 3, 8, 7, 9
-                 1(0)
-               /      \
-             2(-1)      3(+1)
-            /   \      /   \
-         4(-2)  5(0)  6(0)  7(+2)
-                           /    \
-                          8(+1)  9(+3)
+Video: https://www.youtube.com/watch?v=PQKkr036wRc
+Vertical Order Traversal(VOT)?  Nodes printed after traversing the tree vertically.
+
+ How to Print nodes using VOT?
+ - To print nodes in VOT we need to calculate Horizontal_distance(HD) of each node.
+    Why HD? Nodes having same HD fall on same vertical line. NOTE: WE CANNOT DRAW PERFACT BT having nodes falling under 
+    each other perfactly.
+    What is HD? This is horizontal distance of nodes from a imaginary wall placed on left side of tree.
+                        
+    How HD is calculated?
+        HD of root is taken 0
+        if node is left child, its HD = (hd_of_parent - 1)
+        if node is right child, its HD = (hd_of_parent + 1)
+
+Calculating HD of each node on this tree
+                         1
+                      /      \
+                    2         3
+                   /   \      /   \
+                  4     5    6     7
+                                  /   \
+                                 8    9
+                                 
+HD of nodes represented:
+  Imaginary_wall        
+        |                 1(0)
+        |              /      \
+        |            2(-1)      3(+1)
+        |           /   \      /   \
+        |        4(-2)  5(0)  6(0)  7(+2)
+        |                          /    \
+        |                         8(+1)  9(+3)
+        -----------number line------------------>
+
+        HD of root 1 = 0
+        HD of root 2 = 0-1 = -1
+        HD of root 3 = 0+1 = 1
+        HD of root 4 = -1-1 = -2
+
+                HD ->   -2      -1      0       1       2       3
+                --------------------------------------------------
+                nodes->  4       2      1       3       7       9
+                                        5       8
+                                        6
+
+                Vertical_order_traversal = 4 2 1 5 6 3 8 7 9
+
+        This means, Node 4 is nearest to Imaginary_wall, Node 9 is farthest from Imaginary_wall
+        Nodes(1,5,6) fall on same vertical line
+        Nodes(3,8) fall on same vertical line
+
 
 Logic:
-        1. Create and Store values in Hashtable. Hashtable is created using maps container in STL.
-           For every Hd we maintain list of nodes in hashmap.
-           During tree traversal, we calculate Hd and add the node to the hash map using HD as a key in map.
-
-           Take a C++ map<key, value> = map < int, vector<int> >
-                Key = hd of node.
-                value = Array of Integers having same hd.
+1. Calculate and store HD of nodes.   createMapStoringHD_and_NodeVector(Node* root, int hd, map<int, vector<int>> &m)
+   Take map<key,value> to store map<horizontal_distance,(values)>.   map <int, vector<int>> m
+   Store key=horizontal_distance and value=list_of_all_nodes_having_key_as_HD
+      map < int, vector<int> >
+      Key = hd of node.                 -> 0, -1, -2, 1, 2, 3
+      value = Array of values from tree having same hd.
            Hashmap:
                 |       |    |    |     |   |   |
                 |0|1,5,6|-1|2|-2|4|1|3,8|2|7|3|9|
-Map Addresses:  a       a+x  a+y  a+z   a+t
-                -> 0, -1, -2, 1, 2, 3 are keys
-                -> Respective nodes are stored at keys in map.
-          Adding element at hd[-2]:
-                m[-2].push_back(node->data);
+                |       |    |    |     |   |   |
 
-        2. Printing the tree in Vert Order:
-                - Take iterator for printing map. Example:
-                   map :: iterator it;          std::map< int,std::vector<int> > :: iterator it;
-                - Since iterator points to complete map. Traverse whole map using iterator
-                   for (it=m.begin(); it!=m.end(); it++)
-                - keys of map is stored in ascending order.
-                - Print Second element(ie vector) of map using for loop.
-                   for (int i=0; i<it->second.size(); ++i)
+         m[-2].push_back(node->data);       //Adding element at hd[-2]. Map cannot have duplicate keys so, when same key 
+         is encounterd again. push_back() performs pushing back on same key's vector element.
+
+2. Printing the tree in Vert Order:   printVerticalOrder(map<int, vector<int>> &m)
+  - Pass the created map to print fuction.
+  - Take iterator. point it passed map
+  - Print Second element(ie vector) of map using for loop
 */
 
 #include<iostream>
 #include<map>
 #include<vector>
 struct Node{
-        int data;
-        Node *left;
-        Node *right;
+        int key;
+        Node *left, *right;
 };
+
+struct Node* newNode(int key)
+{
+    struct Node* node = new Node;
+    node->key = key;
+    node->left = node->right = NULL;
+    return node;
+}
+
 Node* createTree(){
-        Node *ptr;
-        ptr = new Node;
-        ptr->data = 1;
-        ptr->left = new Node;
-        ptr->left->data = 2;
-        ptr->left->left = new Node;     ptr->left->right = new Node;
-        ptr->left->left->data = 4;
-        ptr->left->left->left = ptr->left->left->right = NULL;
-        ptr->left->right->data = 5;
-        ptr->left->right->left = ptr->left->right->right = NULL;
-        ptr->right = new Node;
-        ptr->right->data = 3;
-        ptr->right->right = new Node;   ptr->right->left = new Node;
-        ptr->right->right->data = 7;
-        ptr->right->right->right = new Node;    ptr->right->right->left = new Node;
-        ptr->right->right->right->data = 9;     ptr->right->right->right->right = ptr->right->right->right->left = NULL;
-        ptr->right->right->left->data = 8;
-        ptr->right->right->left->right = ptr->right->right->left->left = NULL;
-        ptr->right->left = new Node;
-        ptr->right->left->data = 6;     ptr->right->left->right = ptr->right->left->left = NULL;
-        return ptr;
+    Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+    root->right->left->right = newNode(8);
+    root->right->right->right = newNode(9);
+    return root;
 }
 
-void getVertOrder(Node *root, int hd, std::map< int, std::vector<int> > &m){
-        if(!root) return;
+void createMapStoringHD_and_NodeVector(Node* root, int hd, map<int, vector<int>> &m){
+        if (root == NULL)       return;
 
-        m[hd].push_back(root->data);    //map[key]=value;
+        m[hd].push_back(root->key);
 
-        getVertOrder(root->left, hd-1, m);      //store nodes in left subtree
-        getVertOrder(root->right, hd+1, m);     //store nodes in right subtree
+        createMapStoringHD_and_NodeVector(root->left, hd-1, m);
+
+        createMapStoringHD_and_NodeVector(root->right, hd+1, m);
 }
 
-void verticalOrderTraversal(Node *root){
-        std::map < int, std::vector<int> > m;
-        int hd = 0;
-        getVertOrder(root, hd, m);
+void printVerticalOrder(map<int, vector<int>> &m){
+    map<int,vector<int>>::iterator it;
 
-        std::map< int,std::vector<int> > :: iterator it;
-//      std::cout<<"m.size()="<<m.size()<<std::endl;
-
-        for (it=m.begin(); it!=m.end(); it++) {
-//              std::cout<<"it->second.size()="<<it->second.size()<<std::endl; 
-                for (int i=0; i<it->second.size(); ++i)
-                    std::cout << it->second[i] << " ";
-                std::cout << std::endl;
-        }
+    for (it=m.begin(); it!=m.end(); it++)
+    {
+        for (int i=0; i<it->second.size(); ++i)   //first:key, second:value
+            cout << it->second[i] << " ";
+        cout << endl;
+    }
 }
+
 
 int main(){
         Node *root;
         root = createTree();
         std::cout<<"\nTree created root="<<root<<std::endl;
 
-        verticalOrderTraversal(root);
+        int hd = 0;
+  
+        //map<key,value>
+       //key=horizontal_distance, value=vector storing all nodes having 'key' horizontal distance
+        map <int,vector<int>> m;
+        createMapStoringHD_and_NodeVector (root, hd, m);
+
+        printVerticalOrder(m);
         return 0;
 }
 /*
- Input: Above tree's root node
- Output:
+Output:
+Tree created
 4 
 2 
 1 5 6 
