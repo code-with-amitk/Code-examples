c85b48ae5ee66f741d03bec93a2eaf24b652ad21
AmitKumar <amitkumar50@users.noreply.github.com>
2019-12-31 14:54:45 +0530

    Update function_template.cpp

--- c++/templates/function_template.cpp
+++ c++/templates/function_template.cpp
@@ -1,23 +1,19 @@
-#include<iostream>
-
-template <class T>	//T is template variable
-//template <typename T>	//T is template variable. We can use typename also
-T max (T a, T b) {
-	return (a>b) ? a : b;
-}
-
-int main (){
-	int x = 8, y = 9;
-	int z = max (x, y);
-	std::cout<<"z="<<z<<std::endl;
-
-	float x1 = 8.1, y1 = 9.1;
-	float z1 = max (x1, y1);
-	std::cout<<"z1="<<z1<<std::endl;
-
-	return 0;
-}
-/*Output
- * z=9
- * z1=9.1
+/*
+Function template
+ - template <class T>. T is template variable
  */
+#include<iostream>
+using namespace std;
+
+template <class T>                      //OR template <typename T>
+T maximum(T a, T b){
+        return (a>b)?a:b;
+}
+
+int main(){
+        cout<<maximum(4,5)<<endl;               //5
+        cout<<maximum(5.5,7.8)<<endl;           //7.8
+        cout<<maximum(5,7.8)<<endl;             //error: no matching function for call to ‘maximum(int, double)’
+        cout<<maximum('a','b')<<endl;           //b
+        return 0;
+}
