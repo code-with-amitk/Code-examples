ee29a3526a7c61436029a0d4a0d6d7d58049ec96
AmitKumar <amitkumar50@users.noreply.github.com>
2019-12-31 14:53:24 +0530

    Update class_template.cpp

--- c++/templates/class_template.cpp
+++ c++/templates/class_template.cpp
@@ -1,33 +1,41 @@
-#include<iostream>
+/*
+Class template: Generic class which having(Generic member variables, Generic member functions)
+- Gen is Genlate variable
 
-template <class T>
-class addMult {
-	T a, b;
-public:
-	addMult () {}
-	addMult (T x, T y) : a(x), b(y) {}
-	T add () { 
-		return a+b; 
-	}
-	T multiply();
+Rules:
+1. Whenever we define function of template class outside class we need to write template statement again
+2. Compiler cannot deduce template parameter type(s) for class template. We need to tell compiler the data types we would be using.
+3. If arguments are of other type and template argument of other type. Compiler will ignore Argument type.
+ */
+#include<iostream>
+using namespace std;
+
+template <class Gen>            //OR <typename T>
+class test {
+        Gen a, b;               //Generic Member variables
+    public:
+        test (Gen x, Gen y){    //Generic function
+                a = x;
+                b = y;
+        }
+        Gen add (){
+                return a+b;
+        }
+        Gen multiply ();
 };
 
-template <class T>
-T addMult <T>::multiply () { 
-	return a*b;
+template <class Gen>                    //Rule-1
+Gen test <Gen>::multiply(){
+        return a*b;
 }
 
-int main (){
-	addMult <int> obj1 (10,2);
-	std::cout<<"obj1.add()="<<obj1.add()<<std::endl;
+int main(){
+        test <int> obj(5,6);            //Rule-2
+        cout<<obj.add()<<endl;          //Output: 11
 
-	addMult <float> obj2(4.5,2.2);
-	std::cout<<"obj2.multiply()="<<obj2.multiply()<<std::endl;
-	
-	return 0;
+        test <float> obj1(5.6,6.7);
+        cout<<obj1.add()<<endl;         //Output: 12.3
+
+        test <int> obj2(5.6,6.7);       //Rule-3
+        cout<<obj2.multiply()<<endl;    //Output: 30
 }
-/*
- * Output:
- * obj1.add()=12
- * obj2.multiply()=9.9
- */
