23a0d1c6e6f630bd115b7d314d54036188d2259a
AmitKumar <amitkumar50@users.noreply.github.com>
2020-01-14 10:06:43 +0530

    Update maxHeap.cpp

--- data-structures/trees/maxHeap.cpp
+++ data-structures/trees/maxHeap.cpp
@@ -1,4 +1,19 @@
 /*
+What is Binary heap? Balanced complete Binary tree. Duplicates are allowed in heap. 
+Max-Heap?
+Root is always greatest. Condition: parent >= child    //This condition is for all nodes except Root-node.
+
+Question Build a Max-Heap from array {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17}
+Answer 
+               17
+              /   \
+            15     13
+           /  \   /  \
+          9    6  5  10
+         / \  / \
+        4  8 15 1 
+
+
 Basic Logic to Build Max-Heap:
 a. Create Binary tree using level-order-traversal from Array
 b. Perform heapify operation on all non-leaf nodes starting from 1st non-leaf node to top of tree in reverse order.
@@ -10,9 +25,8 @@ Array = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17}
          0  1  2  3  4  5   6   7  8  9   10
 
 Step-1: Create Complete Binary Tree from Array using level-order/bfs method.
-        left-child = 2*i + 1
-        right-child = 2*i + 2
-        For node 4, index=3.    left-child-index=2*3+1=7. left=child=arr[7]=9   right-child-index=2*3+2=8. right-child=arr[8]=8
+        For any node a[i], leftChild = a[2*i + 1], rightChild = a[2*i + 2]
+        Example: i=3 a[i]=4  leftChild=a[2*3+1]=9. rightChild=arr[8]=8
                 
                  1
               /     \
@@ -22,12 +36,11 @@ Step-1: Create Complete Binary Tree from Array using level-order/bfs method.
        / \    / \
       9   8  15 17
 
-Step-2: Total nodes = 11 = N
-        Nodes to heapify = All non-leaf nodes = N/2 - 1 = 11/2 - 1 = 4
-        We need to heapify from index(4) in reverse order.
-        Last non-leaf node = arr[4] = 6
-        To build the heap, heapify only the nodes:
-        [1, 3, 5, 4, 6] in reverse order.
+Step-2: Calculate all non-leaf nodes, on which heapify operation need to be performed!
+        Total nodes size=11, Non-leaf-nodes = sizeOfArray/2 - 1 = 11/2 - 1 = 4
+        We will heapify from last non-leaf node to 1st in reverse order.
+        Last non-leaf node, arr[4]=6.
+        Nodes to heapify=[1, 3, 5, 4, 6] in reverse order.
 
 Step-3: (Heapify 6) Swap 6 and 17. Compare 17,15,6. Place largest element at top
                  1
@@ -75,16 +88,20 @@ Step-7: (Heapify 1) First Swap 1 and 17, again swap 1 and 15, finally swap 1 and
        4   8  3    1
 
 
-Code Logic:
-1. We will rearrange elements within the array itself so that if tree is created using level-order-traversal. it comes out as maxHeap.
-2. Take array having 11 elements. { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17 };.
-        -> We know if we want to build level-order-traversed tree 
-                left-child = 2*i + 1
-                right-child = 2*i + 2
-3. Find index of 1st non-leaf node.     (N/2)-1         (11/2)-1=4
+************Code Logic*************
+1. Calculate number of nodes to be heapified.   size/2 - 1
+
+2. Heapify every node seperately.
+        for(i=size/2-1; i>=0; i--)
+                heapifyANode(a[i]);          //So, we call heapify function for every node seperately
+
+3. How heapifyANode() works?
+
+
 4. Traverse from index 4 to 0 in reverse order, heapify each element. 
         (heapify means: Compare node,it's left & right child. Swap and place largest element at top.)
-        
+
+***********************************
 TIME COMPLEXITY:
 Heapify a single node takes O(Log N) time complexity where N is the total number of Nodes. 
 Therefore, building the entire Heap will take N heapify operations and the total time complexity will be O(N*logN).
@@ -92,8 +109,6 @@ Therefore, building the entire Heap will take N heapify operations and the total
 #include<iostream>
 using namespace std;
 
-// A utility function to print the array
-// representation of Heap
 void printHeap(int arr[], int n)
 {
     cout << "Array representation of Heap is:\n";
@@ -103,18 +118,19 @@ void printHeap(int arr[], int n)
     cout << "\n";
 }
 
-void heapify(int arr[], int noOfElements, int i){
+//This function heapifies every node seperately
+void heapifyANode(int a[], int size, int i){
 
     int parentIndex = i;
-    int leftChildIndex = 2 * i + 1;             //left-child = 2*i + 1
-    int rightChildIndex = 2 * i + 2;    //right-child = 2*i + 2
+    int leftChildIndex = 2*i + 1;             //left-child = 2*i + 1
+    int rightChildIndex = 2*i + 2;            //right-child = 2*i + 2
 
     // If left child is larger than root
-    if (leftChildIndex < noOfElements && arr[leftChildIndex] > arr[parentIndex])
+    if (leftChildIndex < size && arr[leftChildIndex] > arr[parentIndex])
         parentIndex = leftChildIndex;
 
     // If right child is larger than largest so far
-    if (rightChildIndex < noOfElements && arr[rightChildIndex] > arr[parentIndex])
+    if (rightChildIndex < size && arr[rightChildIndex] > arr[parentIndex])
         parentIndex = rightChildIndex;
 
     // If largest is not root
@@ -122,26 +138,26 @@ void heapify(int arr[], int noOfElements, int i){
         swap(arr[i], arr[parentIndex]); //Swap largest element and parentIndex
 
         // Recursively heapify the affected sub-tree
-        heapify(arr, noOfElements, parentIndex);
+        heapifyANode(arr, size, parentIndex);
     }
 }
 
-void buildHeap(int arr[], int noOfElements){
+void buildHeap(int a[], int size){
 
-    int indexOf1stNonLeafNode = (noOfElements / 2) - 1;         //(11/2)-1 = 4. ie arr[4]=6
+    int startIndex = (size / 2) - 1;         //(11/2)-1 = 4. 1st node to heapify
 
-    for (int i = indexOf1stNonLeafNode; i >= 0; i--) {          //Perform heapify operation from 1st nonleaf to root node, in reverse order
-        heapify(arr, noOfElements, i);                  //heapify arr[4], arr[3], arr[2], arr[1] and arr[0]
+    for (int i = startIndex; i >= 0; i--) {  //Perform heapify operation from 1st nonleaf to root node, in reverse order
+        heapify(a, size, i);               //heapify arr[4], arr[3], arr[2], arr[1] and arr[0]
     }
 }
 
 int main(){
-        int arr[] = { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17 };
+        int a[] = { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17 };
 
-        int noofelements = sizeof(arr) / sizeof(arr[0]);        //11
+        int noofelements = sizeof(a) / sizeof(a[0]);        //11
 
-        buildHeap(arr, noofelements);
-        printHeap(arr, noofelements);
+        buildHeap(a, noofelements);
+        printHeap(a, noofelements);
 
         return 0;
 }
