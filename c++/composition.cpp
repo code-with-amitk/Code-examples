df5e9fc333bb09b5f6d6a92975c7e6f42f51004d
AmitKumar <amitkumar50@users.noreply.github.com>
2020-01-23 22:59:59 +0530

    Create composition.cpp

---
+++ c++/composition.cpp
@@ -0,0 +1,41 @@
+/*
+ *      composition.cpp
+ *
+Composition? If container Object is deleted, contained object will also be deleted. 
+- Member variables of class are example of composition.
+
+In below example, A is created But destroyed with B
+*/
+
+#include<iostream>
+using namespace std;
+class A{
+public:
+        A(){
+                cout<<"A's Constructor\n";
+        }
+        ~A(){
+                cout<<"A's Destructor\n";
+        }
+};
+
+class B{
+       A  a;    //Composition
+public:
+       B(){
+               cout<<"B's constructor\n";
+       }
+       ~B(){
+               cout<<"B's Destructor\n";
+       }
+};
+int main(){
+        B b(1);
+}
+/* Output:
+# ./a.out 
+A's Constructor
+B's constructor
+B's Destructor
+A's 
+*/
