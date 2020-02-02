9ffd92a61896f43bcad1ec05db754661921aafec
AmitKumar <amitkumar50@users.noreply.github.com>
2020-02-02 17:44:21 +0530

    Update virtual_destructor.cpp

--- c++/virtual_destructor.cpp
+++ c++/virtual_destructor.cpp
@@ -2,41 +2,59 @@
         virtual_destructor.cpp
  
 Virtual Destructor: 
- Freeing Memory allocated in derived class using Base class pointer, BASE CLASS Destructor needed to be declared virtual.
+Same concept of virtual-function. We have base-class-pointer pointing to derived class object. 
+And we want to delete base-class-pointer then Destructor of derived class will not be called, if not virtual.
 
-- if base class destructor is not virtual:
-        Derv class constructor will not be called.
-Ouput:
-Deleted k
-~B
-~A
-
-- if virtual is not prefixed before base class constructor.
-Output:
-~A
-
+if base class destructor is not virtual: Derv class destructor will not be called.
+-> See commented example below.
 */
+
+/*
 #include<iostream>
 using namespace std;
-
 class A{
+        int *a;
 public:
-        virtual ~A(){
-                cout<<"~A"<<endl;
-        }
+        A(){a = new int;}                               
+        ~A(){cout<<"Destructor class A. (deleting a)"<<endl;}   <<<<< NOT VIRTUAL
 };
 class B: public A{
+        int *b;
 public:
-        ~B(){
-                char *k = new char;
-                delete k;
-                cout<<"Deleted k\n";
-                cout<<"~B"<<endl;
-        }
+        B(){b = new int;}
+        ~B(){cout<<"Destructor class B. (deleting b)"<<endl;}
 };
-
 int main(){
         A *ptr = new B();
         delete ptr;
         return 0;
 }
+
+Output:
+Destructor class A. (deleting a)
+*/
+
+#include<iostream>
+using namespace std;
+class A{
+        int *a;
+public:
+        A(){a = new int;}
+        virtual ~A(){cout<<"Destructor class A. (deleting a)"<<endl;}
+};
+class B: public A{
+        int *b;
+public:
+        B(){b = new int;}
+        ~B(){cout<<"Destructor class B. (deleting b)"<<endl;}
+};
+int main(){
+        A *ptr = new B();
+        delete ptr;
+        return 0;
+}
+/*
+Output:
+Destructor class B. (deleting b)
+Destructor class A. (deleting a)
+*/
