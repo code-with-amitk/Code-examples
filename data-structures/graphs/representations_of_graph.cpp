3665f1f2f5081dc5b99fec49e11573ae5be7df3c
AmitKumar <amitkumar50@users.noreply.github.com>
2020-02-09 14:25:43 +0530

    Update representations_of_graph.cpp

--- data-structures/graphs/representations_of_graph.cpp
+++ data-structures/graphs/representations_of_graph.cpp
@@ -1,24 +1,20 @@
-/*
-	different_ways_to_create_graph.cpp
+//	different_ways_to_create_graph.cpp
 
-Graph to be created:
+/*
+	UNDIRECTED-GRAPH_REPRESENTATION
+	
 	[0] -10- [1] -20- [2]
 	   \	  |	 /
 	   50\ 	  |30  /40
 		 [3] 
-		 
-		 
-***************1. Adjacency-Matrix******************
-1a. Using Arrays
+1a. Using Arrays		//Adjacency-Matrix
 	   0   1   2   3
 	0  0   10  0   50
 	1  10  0   20  30
 	2  0   20  0   40
 	3  50  30  40  0
-*************************************************	
 
-***************2. Adjacency-List******************
-2a. Using vectors
+2a. Using vectors		//Adjacency-List
 v[0] | 0,10 | 3,50 |
 v[1] | 0,10 | 2,20 | 3,30 |
 v[2] | 1,20 | 3,40 |
@@ -27,19 +23,19 @@ v[3] | 0,50 | 1,30 | 2,40 |
 2b. Using Singly LL
 
 2c. Using Doubly LL
-*************************************************
+*/
 
- */
+
 #include<iostream>
 #include<vector>
 #include<forward_list>
 #include<list>
 using namespace std;
 
+#ifdef UNDIRECTED_GRAPH
 typedef pair<int,int> mypair;
-
 int main(){
-	
+
 #ifdef arrays
 	int g[4][4] = {{0,10,0,50},
 		       {10,0,20,30},
@@ -49,7 +45,7 @@ int main(){
 #endif
 	
 #ifdef vector
-	vector<mypair> v[4];
+	vector<mypair> v[4];		//VECTOR
 	v[0].push_back(mypair(0,10));	v[0].push_back(mypair(3,50));
 #endif
 
@@ -61,5 +57,34 @@ int main(){
 #ifdef doubly_ll
 	list<mypair> dll[4];	//Doubly LL
 	dll[0].push_back(mypair(0,10));		dll[0].push_back(mypair(3,50));
+#endif	
+}
+#endif
+
+
+
+/*	DIRECTED_WEIGHTED_GRAPH_REPRESENTATION
+
+	[1] --1---> [2]
+	|
+	1
+	|
+	\/
+	[3]----1---->[4]
+
+1: vector-of-vector/2-D-vector	|src,dst,dis|		//Adjacency-List
+	|     |     |     |
+	|1,2,1|1,3,1|3,4,1|
+	|v[0] |v[1] |v[2] |
+*/
+
+int main(){
+#ifdef VECTOR-OF-VECTOR	
+	vector<vector<int>> v = {{1,2,1},{1,3,1},{3,4,1}};
+	
+	//v.size=3
+        for(int i = 0; i < v.size(); i++)		//Traversing elements
+                for(int j = 0; j < v[j].size(); j++)
+                        cout<<v[i][j]<<" ";		//1 2 1 1 3 1 3 4 1 
 #endif	
 }
