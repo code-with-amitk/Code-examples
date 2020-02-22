e7a2f1382a31e4c39f31b767578bc623f943742c
AmitKumar <amitkumar50@users.noreply.github.com>
2020-02-22 22:39:39 +0530

    Update spiral_print.cpp

--- amazon_questions/arrays/spiral_print.cpp
+++ amazon_questions/arrays/spiral_print.cpp
@@ -1,9 +1,9 @@
-/* Spiral Print of Array
+/*      spiral_print.cpp
+Task: Spiral Print of Array
         1   2   3   4
         5   6   7   8
         9  10  11  12
        13  14  15  16   
-
 O/P->   1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
 
 Logic:
