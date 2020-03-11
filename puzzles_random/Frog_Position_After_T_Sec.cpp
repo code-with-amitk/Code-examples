fc84324997c3b9639f5b73a8079b7b618e8e18b7
AmitKumar <amitkumar50@users.noreply.github.com>
2020-03-11 08:38:41 +0530

    Update Frog_Position_After_T_Sec.cpp

--- puzzles_random/Frog_Position_After_T_Sec.cpp
+++ puzzles_random/Frog_Position_After_T_Sec.cpp
@@ -8,16 +8,23 @@ Situation/Task: Find probability to reach target node.
              / \  \
             4   6  5
 
-Given a undirected tree consisting of n vertices numbered from 1 to n. A frog starts jumping from the vertex 1. In one second, the frog jumps from its current vertex to another unvisited vertex if they are directly connected. The frog can not jump back to a visited vertex. In case the frog can jump to several vertices it jumps randomly to one of them with the same probability, otherwise, when the frog can not jump to any unvisited vertex it jumps forever on the same vertex. 
+Given a undirected tree consisting of n vertices numbered from 1 to n. A frog starts jumping from the vertex 1. 
+In one second, the frog jumps from its current vertex to another unvisited vertex if they are directly connected. 
+The frog can not jump back to a visited vertex. In case the frog can jump to several vertices it jumps randomly 
+to one of them with the same probability, otherwise, when the frog can not jump to any unvisited vertex it jumps 
+forever on the same vertex. 
 
-The edges of the undirected tree are given in the array edges, where edges[i] = [fromi, toi] means that exists an edge connecting directly the vertices fromi and toi.
+The edges of the undirected tree are given in the array edges, where edges[i] = [fromi, toi] means that exists an 
+edge connecting directly the vertices fromi and toi.
 
 Return the probability that after t seconds the frog is on the vertex target.
 
 Example1:
 Input: n = 7, edges = [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]], t = 2, target = 4
 Output: 0.16666666666666666 
-Explanation: The frog starts at vertex 1, jumping with 1/3 probability to the vertex 2 after second 1 and then jumping with 1/2 probability to vertex 4 after second 2. Thus the probability for the frog is on the vertex 4 after 2 seconds is 1/3 * 1/2 = 1/6 = 0.16666666666666666. 
+Explanation: The frog starts at vertex 1, jumping with 1/3 probability to the vertex 2 after second 1 and then jumping 
+with 1/2 probability to vertex 4 after second 2. Thus the probability for the frog is on the vertex 4 after 2 seconds 
+is 1/3 * 1/2 = 1/6 = 0.16666666666666666. 
 
 Example2:
 Input: n = 7, edges = [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]], t = 1, target = 7
