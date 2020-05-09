3b09c646f38cbd965d11f7103bd1fe60d4e326d0
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-09 20:50:34 +0530

    Update arguments_to_function.c

--- assembly/arguments_to_function.c
+++ assembly/arguments_to_function.c
@@ -10,23 +10,6 @@ int main(){
 }
 
 /*
-STEPS:  
-1.Every function starts with epilogue.  -----------------
-   - rbp pushed/stored on stack.        rbp             rsp
-   - rbp=rsp                            -----------------
-                                                      rbp=rsp
-2.Create stack for function                                                   
-   - Move rsp to new position           -------------------------
-            rsp=rsp-16                                  rbp     rsp
-
-3.Store local variables on top of stack  -------------------------
-   mov [rbp-8] 10                                       rbp 10  rsp
-
-4.Function arguments are stored in:
-   rdi,rsi,rdx,rcx,r8......r15
-
-5.Arguments passed to function are stored on stack   
-
 fun:
         push    rbp                     epilogue        //1
         mov     rbp, rsp                epilogue
@@ -45,4 +28,21 @@ main:
         mov     eax, 0
         leave
         ret
+        
+STEPS:  
+1.Every function starts with epilogue.  -----------------
+   - rbp pushed/stored on stack.        rbp             rsp
+   - rbp=rsp                            -----------------
+                                                      rbp=rsp
+2.Create stack for function                                                   
+   - Move rsp to new position           -------------------------
+            rsp=rsp-16                                  rbp     rsp
+
+3.Store local variables on top of stack  -------------------------
+   mov [rbp-8] 10                                       rbp 10  rsp
+
+4.Function arguments are stored in:
+   rdi,rsi,rdx,rcx,r8......r15
+
+5.Arguments passed to function are stored on stack           
 */
