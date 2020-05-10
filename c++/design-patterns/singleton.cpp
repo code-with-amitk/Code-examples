89a4e5576755f208ef56634557ecb60f1963ea35
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-10 22:45:38 +0530

    Update singleton.cpp

--- c++/design-patterns/singleton.cpp
+++ c++/design-patterns/singleton.cpp
@@ -71,22 +71,20 @@ public:
 
 /****C++98 CLASS*******
 class A{
-        A(){}					//1A
-        A(A &){}				//1B
-        void operator = (const A&){}		//1C
-        static A *ptr;				//2
+        A(){}
+        A(A &){}
+        void operator = (const A&){}
+        static A *ptr;
 public:
-        static A *fun(){			//3
+        static A *fun(){
                 if(ptr == NULL)
                         ptr = new A();
                 return ptr;
         }
 };
 *****C++98 CLASS*****/
-//////////////////////////////LAZY INITIALIZATION END///////////////////////////////////
 
 A *A::ptr = nullptr;
-
 int main(){
 //      A obj1;         //Error:error: ‘A::A()’ is private within this context
         A *p1 = A::fun();			//4
@@ -99,3 +97,4 @@ int main(){
 0x1ec5e70
 0x1ec5e70
 */
+//////////////////////////////LAZY INITIALIZATION END///////////////////////////////////
