e3f5a681f68d52eef1811820c47e81469b40ba09
root <root@amitkuma.pnq.csb>
2020-05-12 12:42:47 +0530

    unodered-map-constructors

---
+++ c++/standard_template_library/container_library/associative_containers/unsorted_unordered/unordered_map/um_constructors.cpp
@@ -0,0 +1,20 @@
+/*      uam_creation_insert.cpp
+  
+WHAT? (C++11)
+ - <key,value> pair with unique keys.
+
+COMPLEXITY? 
+ Amortized: O(1)
+ Worst case: O(n)
+
+INTERNALLY:
+        |bucket-1|  |bucket-2|..... |bucket-n|
+ - elements are stored in buckets based on hash value.
+*/
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+
+}
