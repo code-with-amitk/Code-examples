a7829478d4ed1ac14ff9703cbe2c974613fda31b
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-13 13:37:40 +0530

    Update unordered_map_assembly.cpp

--- c++/standard_template_library/container_library/associative_containers/unsorted_unordered/unordered_map/unordered_map_assembly.cpp
+++ c++/standard_template_library/container_library/associative_containers/unsorted_unordered/unordered_map/unordered_map_assembly.cpp
@@ -6,7 +6,14 @@
  0x450                0x200
  rbp                  rsp       
 
-2. Arguments to function are stored in:  rdi, rsi, rdx, rcx, r8...r15 
+2. rdi, rsi, rdx, rcx, r8, r9: Stores 1st 6 arguments to function. If there are more than 6 parameters, 
+   then the program’s stack is used to pass in additional parameters to the function.
+   
+3. rax: stores return value of the function. Larger return types like structs are returned using the stack.
+
+4. rax, rcx, rdx, rdi, rsi, rsp, r8-r11: These are caller save registers. 
+   rbx, rbp, r12-r15: These are callee save registers. These are saved across function calls.   	
+   |- As per telephone system. Caller is main(), callee fun()
 *************************************
 
 **********CTR, DTR CALLED***********
