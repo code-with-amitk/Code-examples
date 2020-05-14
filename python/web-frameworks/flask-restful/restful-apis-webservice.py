337137af6817ab5cbec20b5c7e42a23a22d809f9
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-14 12:41:24 +0530

    Update restful-apis-webservice.py

--- python/web-frameworks/flask-restful/restful-apis-webservice.py
+++ python/web-frameworks/flask-restful/restful-apis-webservice.py
@@ -1,4 +1,6 @@
 """     restful-apis-webservice.py
+https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
+https://gist.github.com/leon-sleepinglion/97bfd34132394e23ca5905ec730f776a
 
 STEPS:
 ********Create virtual Enviornment**********
