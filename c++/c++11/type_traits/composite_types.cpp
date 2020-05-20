2cfe465dcf97a32fafbffe1deabae5d65b06171d
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-20 17:45:22 +0530

    Update composite_types.cpp

--- c++/c++11/type_traits/composite_types.cpp
+++ c++/c++11/type_traits/composite_types.cpp
@@ -10,12 +10,14 @@ is_scalar: checks if a type is a scalar type
 	- cv-qualified arithmetic, pointer, pointer to member, enumeration, or nullptr_t type
 is_object: checks if a type is an object type
 is_compound: checks if a type is a compound type
-	- array, function, object pointer, function pointer, member object pointer, member function pointer, reference, class, union, or enumeration,
+	- array, function, object pointer, function pointer, member object pointer, member function pointer, 
+	reference, class, union, or enumeration,
 is_reference: checks if a type is either a lvalue reference or rvalue reference
 is_member_pointer: checks if a type is a pointer to an non-static member function or object
 
 *****************is_compound********************
-WHAT? Check whether T is array, function, object pointer, function pointer, member object pointer, member function pointer, reference, class, union, or enumeration?
+WHAT? Check whether T is array, function, object pointer, function pointer, member object pointer, member 
+function pointer, reference, class, union, or enumeration?
 Return?
         True, if T satisfies above mentioned types
         Otherwise false
