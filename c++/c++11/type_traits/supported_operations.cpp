86d84b693ed1d5107359b13f38eecb8b593df763
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-20 17:48:27 +0530

    Update supported_operations.cpp

--- c++/c++11/type_traits/supported_operations.cpp
+++ c++/c++11/type_traits/supported_operations.cpp
@@ -5,15 +5,19 @@ WHAT? Tells if type supports the operation or not?
 TYPES?
 is_constructible,is_trivially_constructible,is_nothrow_constructible: 
 	checks if a type has a constructor for specific arguments
-is_default_constructible,is_trivially_default_constructible,is_nothrow_default_constructible:checks if a type has a default constructor
+is_default_constructible,is_trivially_default_constructible,is_nothrow_default_constructible:checks if a 
+type has a default constructor
 is_copy_constructible,is_trivially_copy_constructible,is_nothrow_copy_constructible:checks if a type has a copy constructor
-is_move_constructible,is_trivially_move_constructible,is_nothrow_move_constructible:checks if a type can be constructed from an rvalue reference
-is_assignable,is_trivially_assignable,is_nothrow_assignable:checks if a type has a assignment operator for a specific argument
+is_move_constructible,is_trivially_move_constructible,is_nothrow_move_constructible:checks if a type can be
+constructed from an rvalue reference
+is_assignable,is_trivially_assignable,is_nothrow_assignable:checks if a type has a assignment operator for a
+specific argument
 is_copy_assignable,is_trivially_copy_assignable,is_nothrow_copy_assignable:checks if a type has a copy assignment operator
 is_move_assignable,is_trivially_move_assignable,is_nothrow_move_assignable:checks if a type has a move assignment operator
 is_destructible,is_trivially_destructible,is_nothrow_destructible:checks if a type has a non-deleted destructor
 has_virtual_destructor:checks if a type has a virtual destructor
-is_swappable_with,is_swappable,is_nothrow_swappable_with,is_nothrow_swappable:checks if objects of a type can be swapped with objects of same or different type
+is_swappable_with,is_swappable,is_nothrow_swappable_with,is_nothrow_swappable:checks if objects of a type can be 
+swapped with objects of same or different type
 
 *********is_copy_constructible***********
 WHAT? T is copy_constructible type if:
