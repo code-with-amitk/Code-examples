6b47ac3b0cc323a0b0ccb176d12d8a9c16d6e608
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-25 14:42:48 +0530

    Update function_template.cpp

--- c++/templates/function_templates/function_template.cpp
+++ c++/templates/function_templates/function_template.cpp
@@ -1,22 +1,35 @@
-/* Function template
- - template <class T>. T is template variable
+/*  function_template.cpp
+
+Template is compiler abstraction.
+ 
+Compiler creates the specialization for each type at compile time.
  */
 #include<iostream>
 using namespace std;
 
 template <class T>                      //OR template <typename T>
-T maximum(T a, T b){
+T maxof(T a, T b){
         return (a>b)?a:b;
 }
 
+/*
+//Specialization build for int
+int maxof(int a, int b){
+        return (a>b)?a:b;
+}
+
+//Specialization build for string
+string maxof(string a, string b){
+        return (a>b)?a:b;
+}
+*/
+
 int main(){
-        cout<<maximum(4,5)<<endl;               //5
+        cout << maxof(4,5) <<endl;               //5
  
-        cout<<maximum(5.5,7.8)<<endl;           //7.8
+        cout << maxof(5,7.8) <<endl;             //error: no matching function for call to ‘maximum(int, double)’
  
-        cout<<maximum(5,7.8)<<endl;             //error: no matching function for call to ‘maximum(int, double)’
- 
-        cout<<maximum('a','b')<<endl;           //b
+        cout << maxof("hellome","hello") <<endl;           //hellome
  
         return 0;
 }
