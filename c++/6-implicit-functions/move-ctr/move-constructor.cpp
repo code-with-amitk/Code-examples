f27e15169e066c0ba237d8c9808b8bbc0dc45568
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-27 20:07:58 +0530

    Update move-constructor.cpp

--- c++/6-implicit-functions/move-ctr/move-constructor.cpp
+++ c++/6-implicit-functions/move-ctr/move-constructor.cpp
@@ -1,7 +1,7 @@
 /*	move-constructor.cpp
 WHAT? 
   - same named function of class A
-  - Takes non-const R-value reference as parameter
+  - Takes non-const R-value reference as parameter.
   - Internally implemented as:
     - For union-type(https://en.cppreference.com/w/cpp/language/union) as std::memmove()
         void* memmove( void* dest, const void* src, std::size_t count );
@@ -11,8 +11,7 @@ WHAT?
 WHEN MOVE CTR IS CALLED?
   - object is initialized (by direct-initialization or copy-initialization) from rvalue
 
-WHEN MOVE CTR IS NEVER CALLED?
- - When the initializer is a prvalue. see copy elision.  
+WHEN MOVE CTR IS NEVER CALLED? When the initializer is a prvalue. see copy elision.  
 
 THINGS TO CONSIDER?
  - After move original object can be empty, But c++ standards says this should not be relied upon.
@@ -36,10 +35,19 @@ FORCING COMPLIER TO CREATE DEFAULT ASSIGNMENT OPERATOR
 
 
 /*************USER CREATED MOVE CTR*****************/
+///////////////EXAMPLE-1////////////////////////////
 struct A{
         string a;
         A() : a("test"){}
-        A(A& k):a(k.a){cout<<"move failed";}
+        A(A& k):a(k.a){
+          cout<<"move failed";
+        }
+
+        /*Why noexcept is required in move Constructor?
+        noexcept will prevent exceptions when source object is left in unknown 
+        state after move.
+        Compiler will ignore move ctr, if exception happened and noexcept is preceded.
+        */
         A(A &&k) noexcept: a(move(k.a)){}
 };
 
@@ -47,10 +55,46 @@ int main(){
         A obj1 = A();                           //Called default ctr
         cout<<"obj1.a = "<<obj1.a<<"\n";        //obj1.a = test
 
+        /*C++ provides a template function std::move(), which 
+        tells compiler to move value from src to dst if its R-VALUE.
+        You must be thinking, obj1 is lvalue then how this qualifies as r-value?
+        std::move() just takes Reference and casts as rvalue reference.
+        Internally,        move(obj1) = (A &&) v1;
+        */        
         A obj2 = move(obj1);
 
+        //quoted() just puts quotes around output
         cout<<"obj1.a = "<<quoted(obj1.a)<<"\n";        //obj1.a = ""
         cout<<"obj2.a = "<<quoted(obj2.a)<<"\n";        //obj2.a = "test"
 }
-//quoted() just puts quotes around output
 
+///////EXAMPLE-2: MOVING VECTOR USING std::move()/////////
+void disp(vector<string> &v){
+    if(!v.size())
+            cout<<"Empty";
+  
+    for (auto i:v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+int main() {
+
+    vector<string> v1 = {"one", "two", "three"};
+
+    /*You must be thinking, v1 is lvalue then how this qualifies as r-value?
+    std::move() just takes Reference and casts as rvalue reference.
+    move(v1) = (vector<string> &&) v1;
+    */
+    auto v2 = move(v1);
+    disp(v1);
+    disp(v2);
+    return 0;
+}
+/*Output:
+# ./a.out 
+Empty
+one two three
+*/
+/*********************************************************/
