6dcef2b453a7ce0d5541762a430cd68d1520a164
AmitKumar <amitkumar50@users.noreply.github.com>
2020-05-29 17:49:14 +0530

    Create function_returns_rvalue.cpp

---
+++ c++/6-implicit-functions/move-ctr/function_returns_rvalue.cpp
@@ -0,0 +1,119 @@
+/*
+Function returns a temporary value in Register(rax).
+Temporary value is R-value.
+Copying object using R-value calls move ctr.
+*/
+
+/************EXAMPLE-1: Function returning R-value*******************
+To understand steps: See Assembly below.
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class A{
+        int a;
+public:
+        A(int b):a(b)
+        {
+                cout<<"Default ctr\n";
+        }
+        A(A &k){
+                cout<<"copy ctr\n";
+                a = k.a;
+        }
+        A(A &&k) noexcept{
+                a = move(k.a);
+                cout<<"move ctr\n";
+        }
+};
+
+/*Function returns a temporary unnamed value, which is definition of R-value
+*/
+A fun(A o)
+{
+        return o;
+}
+
+int main(){
+    A obj2 = fun(A(1));
+}
+/*
+Output:
+# ./a.out 
+Default ctr
+move ctr
+*/
+
+/*ASSEMBLY
+A::A(A&&):
+        call    std::remove_reference<int&>::type&& std::move<int&>(int&)
+        
+fun(A):
+        call    A::A(A&&)
+        
+main:
+        call    A::A(int)                     //A(1)
+        call    fun(A)                        //fun(A(1))
+*/
+/*******************************************************************/
+
+
+/************EXAMPLE-2: Function returning R-value*******************/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class A{
+        int a;
+public:
+        A(int b):a(b)
+        {
+                cout<<"Default ctr\n";
+        }
+        A(A &k){
+                cout<<"copy ctr\n";
+                a = k.a;
+        }
+        A(A &&k) noexcept{
+                a = move(k.a);
+                cout<<"move ctr\n";
+        }
+};
+
+//Note (A o) calls copy ctr because Copying Object using L value
+A fun(A o)      
+{
+        return o;
+}
+int main(){
+        A obj1(1);
+
+        //obj1 is created ie its L value
+        A obj3 = fun(obj1);       //Equal to calls move ctr
+}
+/*
+Output:
+# ./a.out 
+Default ctr
+copy ctr
+move ctr
+*/
+
+/*ASSEMBLY
+A::A(A&&):
+        call    std::remove_reference<int&>::type&& std::move<int&>(int&)
+        
+A::A(A&):
+        call    basic_ostream<char, char_traits<char> >& operator<< <char_traits<char> >
+                (basic_ostream<char, char_traits<char> >&, char const*)
+        
+fun(A):
+        call    A::A(A&&)
+        
+main:
+        call    A::A(int)                     //A obj1(1);
+        call    A::A(A&)                      //fun(A o) = fun(obj1).Passing L-value and hence copy ctr called
+        call    fun(A)        
+        
+/*******************************************************************/
