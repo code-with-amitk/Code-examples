9cf25e6ca1ec3e2568ae5b79275bb02791e70a8d
amit <amitkumar25nov1@gmail.com>
2020-06-16 20:03:34 +0530

    temp-java

---
+++ c++/design-patterns/Behavioral/Template/java/Coffee.java
@@ -0,0 +1,9 @@
+public class Coffee extends beverageMaker {    
+	public void addBase() {        
+		System.out.println("Adding Coffee");    
+	}    
+
+	public void addCondiments() {        
+		System.out.println("Adding Sugar and Milk");    
+	} 
+}
---
+++ c++/design-patterns/Behavioral/Template/java/Tea.java
@@ -0,0 +1,10 @@
+public class Tea extends beverageMaker {    
+	public void addBase() {        
+		System.out.println("Adding tea");    
+	}    
+
+	public void addCondiments() {        
+		System.out.println("Adding Lemon");    
+	} 
+}
+
