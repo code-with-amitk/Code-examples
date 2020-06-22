f9d01f7114db842fa85d1461bd827bc47a71d980
AmitKumar <amitkumar50@users.noreply.github.com>
2020-06-22 08:44:27 +0530

    Update unpairedElementInArray.cpp

--- ds_questions/questions/vectors_arrays/arrays/unpairedElementInArray.cpp
+++ ds_questions/questions/vectors_arrays/arrays/unpairedElementInArray.cpp
@@ -1,5 +1,7 @@
 /*
-A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+A non-empty array A consisting of N integers is given. 
+The array contains an odd number of elements, and each element of the array can be paired with another 
+element that has the same value, except for one element that is left unpaired.
 
 For example, in array A such that:
 
