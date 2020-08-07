6f2587113a37de67016904669cdc45902da700f9
AmitKumar <amitkumar50@users.noreply.github.com>
2020-08-07 16:11:45 +0530

    Update composite_types.cpp

--- Languages/programming_lang/c++/c++11/type_traits/composite_types.cpp
+++ Languages/programming_lang/c++/c++11/type_traits/composite_types.cpp
@@ -3,17 +3,7 @@ https://en.cppreference.com/w/cpp/types
 
 WHAT? Composite data types check.
 
-TYPES?
-is_fundamental: checks if a type is a fundamental type(void or nullptr_t)
-is_arithmetic: checks if a type is an arithmetic type
-is_scalar: checks if a type is a scalar type
-	- cv-qualified arithmetic, pointer, pointer to member, enumeration, or nullptr_t type
-is_object: checks if a type is an object type
-is_compound: checks if a type is a compound type
-	- array, function, object pointer, function pointer, member object pointer, member function pointer, 
-	reference, class, union, or enumeration,
-is_reference: checks if a type is either a lvalue reference or rvalue reference
-is_member_pointer: checks if a type is a pointer to an non-static member function or object
+
 
 *****************is_compound********************
 WHAT? Check whether T is array, function, object pointer, function pointer, member object pointer, member 
@@ -36,19 +26,4 @@ SYNTAX:
 #include<type_traits>
 using namespace std;
 
-int main(){
-	class test{};
-    	cout << (is_compound<test>::value
-                     ? "T is compound"
-                     : "T is not a compound") << '\n';
 
-    	cout << (is_compound<int>::value
-                     ? "T is compound"
-                     : "T is not a compound") << '\n';	
-}
-/*
-Output:
-# ./a.out 
-T is compound
-T is not a compound
-*/
