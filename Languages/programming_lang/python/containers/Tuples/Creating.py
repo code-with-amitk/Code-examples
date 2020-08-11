1f4180688156f0cebed9807e56cc91c0edbe9ed9
AmitKumar <amitkumar50@users.noreply.github.com>
2020-08-12 01:11:02 +0530

    Update and rename Languages/programming_lang/python/containers/tuples.py to Languages/programming_lang/python/containers/Tuples/Creating.py

---
+++ Languages/programming_lang/python/containers/Tuples/Creating.py
@@ -0,0 +1,8 @@
+## Creating Tuples
+
+### 1. Create tuple after taking input from user
+```
+a = input("Input some comma seprated numbers : ")     #1,2,3,4
+t = tuple(a)
+print('Tuple:',t)                                     #(1,2,3,4)
+```
