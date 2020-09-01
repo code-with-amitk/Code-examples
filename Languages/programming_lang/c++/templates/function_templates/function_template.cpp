7903f432a42cdbbeb80c760ebd9084b57aeda348
AmitKumar <amitkumar50@users.noreply.github.com>
2020-09-02 01:07:52 +0530

    Update function_template.cpp

--- Languages/programming_lang/c++/templates/function_templates/function_template.cpp
+++ Languages/programming_lang/c++/templates/function_templates/function_template.cpp
@@ -1,41 +1,23 @@
-/*  function_template.cpp
-
-Template is compiler abstraction.
- 
-Compiler creates the specialization for each type at compile time.
-Means Seperate specializations(overloaded functions) for:
-- long int, short int, int, double, string
-
-Specialization
-Benefits: Makes code type-safe.    
-Drawbacks: increases sizeof object file
-*/
-#include<iostream>
-using namespace std;
-
-template <class T>                      //OR template <typename T>
-T maxof(T a, T b){
-        return (a>b)?a:b;
+## Function Templates
+- Template is compiler abstraction.
+- Compiler creates the specialization for each type at compile time. Means Seperate specializations(overloaded functions) for:
+  - long int, short int, int, double, string
+- Specialization
+  - Benefits: Makes code type-safe.    
+  - Drawbacks: increases sizeof object file
+```c++
+//At compile time, compiler creates 2 overloaded versions of maxof function.
+//maxof(int a, int b){ }
+//maxof(const char*, const char*){  }
+   
+template <class T>
+T maxof(T a, T b) {
+  return (a>b)?a:b;
 }
 
-/*
-//Specialization build for int
-int maxof(int a, int b){
-        return (a>b)?a:b;
+int main() {
+ cout << maxof(4,5) <<endl;                 //Calls as maxof<int> (4,5)
+ //cout << maxof(5,7.8) <<endl;             //error: no matching function for call to ‘maximum(int, double)’
+ cout << maxof("hellome","hello") <<endl;   //hellome.  calls as maxof<const char*>("hellome", "hello")
 }
-
-//Specialization build for string
-string maxof(string a, string b){
-        return (a>b)?a:b;
-}
-*/
-
-int main(){
-        cout << maxof(4,5) <<endl;               //5
- 
-        cout << maxof(5,7.8) <<endl;             //error: no matching function for call to ‘maximum(int, double)’
- 
-        cout << maxof("hellome","hello") <<endl;           //hellome
- 
-        return 0;
-}
+```
