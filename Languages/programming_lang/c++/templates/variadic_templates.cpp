16aa11ca0d4f28d970b968e9efa70dac147228b6
AmitKumar <amitkumar50@users.noreply.github.com>
2020-09-02 00:04:13 +0530

    Update variadic_templates.cpp

--- Languages/programming_lang/c++/templates/variadic_templates.cpp
+++ Languages/programming_lang/c++/templates/variadic_templates.cpp
@@ -1,38 +1,31 @@
-/*  variadic_template.cpp
+## Variadic Templates
+-  A template with at least one parameter pack or Template taking variable number of arguments.
+- **Types of VT**
+  - *1.* Variadic Function template
+  - *2.* Variadic class template
 
-https://eli.thegreenplace.net/2014/variadic-templates-in-c/
-
-VARIADIC TEMPLATE? A template with at least one parameter pack or Template taking 
-variable number of arguments.
-
-TYPES OF VT:
- 1. VARIADIC FUNCTION TEMPLATE
- 2. VARIADIC CLASS TEMPLATE:
-*/
-
-#include<iostream>
-using namespace std;
-
-/*A1.VARIADIC FUNCTION TEMPLATE: Function template taking variable no of arguments. Parameter pack may appear at any place.
-*/
+### Example
+- **1. Variadic Function template**
+  - Function template taking variable no of arguments. Parameter pack may appear at any place.
+```c++
 template<typename ... T>
 void fun(T ... args){
-        (cout << ... << args);
+  (cout << ... << args);
 }
+```
 
-/*A2. VARIADIC CLASS TEMPLATE: class template taking variable no of arguments. template parameter pack must be the final 
-parameter in the template parameter list else its compile time error.
-*/
+- **2. Variadic class template**
+- class template taking variable no of arguments. template parameter pack must be the final parameter in the template parameter list else its compile time error.
+```
 template<typename ... T>
 struct A {};
-
 int main(){
-        fun();       // OK: args contains no arguments
-        fun(1);      // OK: args contains one argument: int
-        fun(2, 1.0); // OK: args contains two arguments: int and double
+  fun();       // OK: args contains no arguments
+  fun(1);      // OK: args contains one argument: int
+  fun(2, 1.0); // OK: args contains two arguments: int and double
 
-         A<> t0;
-         A<int> t1;        // Types contains one argument: int
-         A<int, float> t2; // Types contains two arguments: int and float       
-
+  A<> t0;
+  A<int> t1;        // Types contains one argument: int
+  A<int, float> t2; // Types contains two arguments: int and float       
 }
+```
