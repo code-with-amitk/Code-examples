c4089b1504802fdaa7a9538203722c1a0ed3861a
AmitKumar <amitkumar50@users.noreply.github.com>
2020-10-04 19:43:21 +0530

    Update chain_of_responsibility.cpp

--- Languages/programming_lang/c++/design-patterns/Behavioral/Chain-of-Resp/chain_of_responsibility.cpp
+++ Languages/programming_lang/c++/design-patterns/Behavioral/Chain-of-Resp/chain_of_responsibility.cpp
@@ -1,15 +1,16 @@
-/*      chain_of_responsibility.cpp
+## Chain of Responsibilty
+- **What?** Request is processed by 1 or more objects in chain. 
+- **Logic?**
+  - Take a Abstract class, which will overriden by Concrete classes
+  - Override Handler Interface using Concrete classes
+    - Each concrete class will hold pointer to Object of next class
+    - Responsibility is passed by calling function from other object using pointer.
 
-What? Request is processed by 1 or more objects in chain. 
-
-***************Logic*****************
-- Take a Abstract class, which will overriden by Concrete classes
-- Override Handler Interface using Concrete classes
-        - Each concrete class will hold pointer to Object of next class
-        - Responsibility is passed by calling function from other object using pointer.
-*************************************
-*/
-
+### Code
+- **Pure Virtual Func**
+  - Cannot have body in Abstract class
+  - Must be overridden in derived class.
+```c++
 #include <iostream>
 using namespace std;
 
@@ -17,53 +18,51 @@ int limit_per_object = 50;
 
 class Base {                    //ABSTRACT CLASS. Handler Interface
 protected:
-        Base *next;
+  Base *next;
 public:
-        Base(){next = NULL;}
-        virtual void request(int value) = 0;    //Pure virtual Function.
-        void setNextHandler(Base *p){
-            this->next = p;
-        }
+  Base(){next = NULL;}
+  virtual void request(int value) = 0;    //Pure virtual Function.
+  void setNextHandler(Base *p){
+    this->next = p;
+  }
 };
 
 class derv : public Base{       //Concrete class
-        int myId;
+  int myId;
 public:
-        derv(int a){
-                myId = a;
-        }
+  derv(int a): myId(a) {}
 
-        void request(int value){
-            if(value > limit_per_object) {
-                    cout<<"Object"<<this->myId<<" handling ["<<limit_per_object<<"] requests\n";
+  void request(int value){
+    if(value > limit_per_object) {
+      cout<<"Object"<<this->myId<<" handling ["<<limit_per_object<<"] requests\n";
 
-                    if(this->next != NULL){
-                            this->next->request(value - limit_per_object);
-                    } else {
-                        cout << "I am the last handler Object"<<this->myId<<" handling ["<<limit_per_object << "] requests.\n" << endl;
-                        if(value - limit_per_object)
-                                cout<<"Sorry your ["<<(value - limit_per_object)<<"] requests are still pending\n";
-                    }
-            }
-        }
+      if(this->next != NULL){
+        this->next->request(value - limit_per_object);
+      } else {
+        cout << "I am the last handler Object"<<this->myId<<" handling ["<<limit_per_object << "] requests.\n" << endl;
+        if(value - limit_per_object)
+          cout<<"Sorry your ["<<(value - limit_per_object)<<"] requests are still pending\n";
+      }
+    }//if
+  }
 };
 
 int main () {
-    Base *obj1 = new derv(1);
-    Base *obj2 = new derv(2);
-    Base *obj3 = new derv(3);
+  Base *obj1 = new derv(1);
+  Base *obj2 = new derv(2);
+  Base *obj3 = new derv(3);
 
-    obj1->setNextHandler(obj2);
-    obj2->setNextHandler(obj3);
-    cout<<"\nResponsibility Chain Set\n\n";
+  obj1->setNextHandler(obj2);
+  obj2->setNextHandler(obj3);
+  cout<<"\nResponsibility Chain Set\n\n";
 
-    obj1->request(180);
+  obj1->request(180);
 
-    delete obj1;
-    delete obj2;
-    delete obj3;
+  delete obj1;
+  delete obj2;
+  delete obj3;
 
-    return 0;
+  return 0;
 }
 
 /*
@@ -80,3 +79,4 @@ I am the last handler Object3 handling [50] requests.
 Sorry your [30] requests are still pending
 
 */
+```
