4b2ff26936afdc25871cdae0274ff2ad98a33e15
AmitKumar <amitkumar50@users.noreply.github.com>
2021-05-19 06:43:58 +0530

    Update Iterative.cpp

--- DS_Questions/Data_Structures/Graphs/Questions/Traversals/BFS/Iterative.cpp
+++ DS_Questions/Data_Structures/Graphs/Questions/Traversals/BFS/Iterative.cpp
@@ -1,9 +1,7 @@
 /*	breadth_1st_traversal_iterative.cpp		//Amit Read Algo. Whole day is spent on this.
 
 Task: Print nodes of graph in BFS fashion, where starting node is 0
-Graph: 	0 -- 1 -- 2		//4 Nodes, 5 edges
-	 \  |	/
-	    3
+
         
 Method-1: Adjacency Matrix:
 	 0  1  2  3
@@ -29,68 +27,11 @@ Method-2: Adjacency-list	//USED HERE
   b. Take a queue.
      - Push starting node on queue.	q.push(0)
      
-  c. Perform these until queue is not empty
-     - u = Pop_top_of_queue
-     - print u
-     - Iterate through all neighbours of u
-     	if(neighbour ! visited)
-		mark visited 
-		push neighbour on queue
+
 		
-Complexity:O(V+E)		
-V=No of vertices,  E=No of edges
+Complexity
 *******************************************
 https://www.geeksforgeeks.org/bfs-using-stl-competitive-coding/      
  */
 
-void BFS(vector <int> g[], int startingNode){
-        bool visited[4]={false};
 
-        queue<int> q;
-        int u;
-
-        q.push(startingNode);           //Push 0
-
-        visited[startingNode] = true;      //Mark 0 as visited
-
-        while(q.empty() != 1){
-
-                u = q.front();       q.pop();
-               cout<< u << " ";
-
-                for(auto i = g[u].begin(); i != g[u].end(); i++){ //Traverse popped Node's Neighbours
-
-                        //*i is entry in Adjacency list, 0 -> 1,3. Connected nodes
-
-                        if(visited[*i] == 0){                     //For all unvisited neighbours
-
-                                visited[*i] = true;               //Mark retrieved node as visited.
-
-                                q.push(*i);                            //Push neighbour on queue.
-
-                        }
-                }
-        }
-}
-
-int main()
-{
-        vector<int> v[4];               //Vector storing Adjacecy Matrix of graph
-
-        v[0].push_back(1);      v[0].push_back(3);
-        v[1].push_back(0);      v[1].push_back(2);      v[1].push_back(3);
-        v[2].push_back(1);      v[2].push_back(3);
-        v[3].push_back(0);      v[3].push_back(1);      v[3].push_back(2);
-
-        cout<<"BFS Traversal\n";
-        BFS(v, 0);
-
-        return 0;
-}
-
-/*
-Output:
-# ./a.out
-BFS Traversal
-0 1 3 2
-*/
