5602769826438fe1b03c0e4b706ea495d149cc9b
AmitKumar <amitkumar50@users.noreply.github.com>
2021-07-15 15:15:43 +0530

    Update static_func_variable.cpp

--- Languages/Programming_Languages/c++/Keywords/static/static_func_variable.cpp
+++ Languages/Programming_Languages/c++/Keywords/static/static_func_variable.cpp
@@ -4,26 +4,7 @@ A. STATIC MEMBER VARIABLES:
     - Shared among all Objects of class. Only 1 copy of variables is created.
     - Should be initialized before using the variable.
 
-B. MEMBER FUNCTION: Can be called without class object
+B. 
 */
 
-#include<iostream>
-using namespace std;
 
-class A{
-        static int a;           //A
-public:
-        static void f(){
-                a = 20;
-                cout<<"Hello="<<a<<endl;
-        }
-};
-
-int A::a=10;                    //A
-
-int main(){
-        //B
-        A::f();         //O/P: Hello
-
-        return 0;
-}
