08b1f9459a3a382d1790e7d3f1133c6a68d39eaa
AmitKumar <amitkumar50@users.noreply.github.com>
2021-08-09 17:51:42 +0530

    Update forward_iterator.cpp

--- Languages/Programming_Languages/c++/Standard_Template_Library/iterator_library/forward_iterator/forward_iterator.cpp
+++ Languages/Programming_Languages/c++/Standard_Template_Library/iterator_library/forward_iterator/forward_iterator.cpp
@@ -1,7 +1,6 @@
 /*
 WHAT?
- - Combination of input & output iterator.
- - Allowed to move forward not backward.
+
 
 OPERATIONS:
 1. INCREAMENT: ok
@@ -9,40 +8,4 @@ OPERATIONS:
 	- forward iterator class have not implemented decrement operator
 */
 #include <iostream>
-#include<forward_list>
 
-using namespace std;
-
-int main(){
-	/* EXAMPLE-1: forward_list uses forward_iterator
-	 * forward_list is single LL
-	 * & provides forward iterator.
-	 * iterator can iterate from front to end not from end to start
-	 */
-	forward_list<int> a = {1,2,3,4,5};
-	forward_list<int>::iterator it;
-
-
-	/* DECREMENTING: not allowed
-	   - if we try to decrement forward iterator it gives error at compile time
-	 */
-	//it = a.end();
-	//--it;			//error: no match for ‘operator--’ (operand type is ‘std::forward_list<int>::iterator’ {aka ‘std::_Fwd_list_iterator<int>’})
-
-	for(it=a.begin();it!=a.end();it++){
-		cout<<*it<<" ";
-	}
-	cout<<endl;
-
-
-	/* EXAMPLE-2: range-based loops uses forward iterator
-	 */
-	for(int i:a)
-		cout<<i<<" ";
-
-}
-/*
-# ./a.out 
-1 2 3 4 5 
-1 2 3 4 5
-*/
