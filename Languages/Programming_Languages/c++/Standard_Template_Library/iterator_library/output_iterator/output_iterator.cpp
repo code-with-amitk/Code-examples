10f662ac24316dabff56d140681671b90dbbb495
AmitKumar <amitkumar50@users.noreply.github.com>
2021-08-09 17:49:04 +0530

    Update output_iterator.cpp

--- Languages/Programming_Languages/c++/Standard_Template_Library/iterator_library/output_iterator/output_iterator.cpp
+++ Languages/Programming_Languages/c++/Standard_Template_Library/iterator_library/output_iterator/output_iterator.cpp
@@ -7,22 +7,7 @@ OUTPUT ITERATOR?
 #include<iterator>
 using namespace std;
 
-int main(){
 
-	/*EXAMPLE:
-	  ostream container provides output iterator
-	 */
-	ostream_iterator<int> test(cout," ");
-
-	for(int i=0;i<10;i++)
-		*test++=i;
-
-	cout<<endl;
-}
-/* O/P:
-# g++ output_iterator.cpp 
-# ./a.out 
-0 1 2 3 4 5 6 7 8 9 
 
 */
 
