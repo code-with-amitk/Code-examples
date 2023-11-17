9889b127295bc7afcab20717eda066cc796d0ef6
AmitKumar <amitkumar50@users.noreply.github.com>
2023-11-17 14:07:34 +0530

    Create script.js

---
+++ Languages/ScriptingLanguages/JavaScript/JavaScript_Lang/Code/Add_JS_on_HTML_Page/script.js
@@ -0,0 +1,11 @@
+function createParagraph() {
+  const para = document.createElement("p");
+  para.textContent = "You clicked the button!";
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
+}
