0bb28e7f39c384f4154b24dad520b354e1286a50
AmitK <23084104+code-with-amitk@users.noreply.github.com>
2025-01-05 23:04:21 +0530

    Update index.tsx

--- Projects/React-Native/StickerSmash/app/index.tsx
+++ Projects/React-Native/StickerSmash/app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+// Add StyleSheet to create a styles object to define our custom styles
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Amit's 1st App</Text>
     </View>
   );
 }
+
+/* https://reactnative.dev/docs/stylesheet
+Stylesheets in React-Native
+StyleSheet is an abstraction similar to CSS StyleSheets.
+*/
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
